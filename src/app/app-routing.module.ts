c31bfcb4c341752bce1647e621f465b44e28ee8a
konradst00 <starkombik@gmail.com>
2022-04-05 11:25:04 +0200

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProfileComponent} from './Components/profile/profile.component';
+import {MainWallComponent} from './Components/main-wall/main-wall.component';
+import {NotFoundComponent} from './Components/not-found/not-found.component';
+import {PostDetailsComponent} from './Components/post-details/post-details.component';
+import {LoginRegistrationComponent} from './Components/login-registration/login-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainWallComponent },
+  { path: '404', component: NotFoundComponent},
+  { path: 'login', component: LoginRegistrationComponent},
+  { path: 'registration', component: LoginRegistrationComponent},
+  { path: 'p/:id', component: PostDetailsComponent },
+  { path: '**', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [MainWallComponent, ProfileComponent, NotFoundComponent, LoginRegistrationComponent];
